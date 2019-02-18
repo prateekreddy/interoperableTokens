47ec899985584956c39cc7b725f9d8bc46c6a730
Prateek Reddy <prateekreddyy@gmail.com>
2019-02-18 15:35:27 +0530

    small changes

--- test/test.js
+++ test/test.js
@@ -3,8 +3,9 @@ const config = require('../config.json');
 const token1MetaData = require("../build/contracts/Token1.json");
 const token2MetaData = require("../build/contracts/Token2.json");
 const interopMetaData = require("../build/contracts/Interoperability.json");
-
-const web3 = new Web3(new Web3.providers.WebsocketProvider(`ws://${config.host}:${config.port}`));
+const EthereumTx = require('ethereumjs-tx')
+console.log(`connecting to ${config.host}`);
+const web3 = new Web3(new Web3.providers.WebsocketProvider(`${config.host}`));
 
 const token1 = new web3.eth.Contract(token1MetaData.abi, config.token1Address);
 const token2 = new web3.eth.Contract(token2MetaData.abi, config.token2Address);
@@ -14,16 +15,47 @@ const initialUser1Balance = 1000;
 const initialInteropToken2Balance = 10000;
 const tokenInteropTransferValue = 200;
 
-function sendToken1(toAddress, tokenAmount) {
-    return token1.methods.transfer(toAddress, tokenAmount).send({
-        from: config.owner
-    });
+async function createRawTx(userPrivKey, userAddress, contractAddress, callData) {
+    const privateKey = Buffer.from(userPrivKey.replace('0x', ''), 'hex');
+    console.log("getting tx count for "+userAddress);
+    let nonce = await web3.eth.getTransactionCount(userAddress);
+    console.log(web3.utils.toHex(nonce));
+    const txParams = {
+        nonce: web3.utils.toHex(nonce),
+        gasPrice: '0x09', 
+        gasLimit: '0x271000',
+        to: contractAddress, 
+        value: '0x00', 
+        data: callData,
+        // EIP 155 chainId - mainnet: 1, ropsten: 3
+        chainId: 3
+    }
+
+    const tx = new EthereumTx(txParams)
+    tx.sign(privateKey)
+    const serializedTx = tx.serialize()
+    console.log(serializedTx);
+    return serializedTx;
 }
 
-function sendToken2(toAddress, tokenAmount) {
-    return token2.methods.transfer(toAddress, tokenAmount).send({
-        from: config.owner
-    });
+async function sendToken1(toAddress, tokenAmount) {
+    // return token1.methods.transfer(toAddress, tokenAmount).send({
+    //     from: config.owner
+    // });
+    const callData = token1.methods.transfer(toAddress, tokenAmount).encodeABI();
+    const rawTx = await createRawTx(config.ownerPrivKey, config.owner, config.token1Address, callData);
+
+    console.log("0x"+rawTx.toString('hex'));
+    console.log("sending token1 to some address");
+    return web3.eth.sendSignedTransaction("0x"+rawTx.toString('hex'));
+}
+
+async function sendToken2(toAddress, tokenAmount) {
+    const callData = token2.methods.transfer(toAddress, tokenAmount).encodeABI();
+    const rawTx = await createRawTx(config.ownerPrivKey, config.owner, config.token2Address, callData);
+
+    console.log("0x"+rawTx.toString('hex'));
+    return web3.eth.sendSignedTransaction("0x"+rawTx.toString('hex'));
 }
 
 function checkToken1Balance(address) {
@@ -34,10 +66,31 @@ function checkToken2Balance(address) {
     return token2.methods.balanceOf(address).call();
 }
 
-function approveToken1(user, spender, tokenAmount) {
-    return token1.methods.approve(spender, tokenAmount).send({
-        from: user
-    });
+async function approveToken1(user, userPrivKey, spender, tokenAmount) {
+    const callData = token1.methods.approve(spender, tokenAmount).encodeABI();
+    const rawTx = await createRawTx(userPrivKey, user, config.token1Address, callData);
+
+    console.log("0x"+rawTx.toString('hex'));
+    return web3.eth.sendSignedTransaction("0x"+rawTx.toString('hex'));
+    // return token1.methods.approve(spender, tokenAmount).send({
+    //     from: user
+    // });
+}
+
+async function interoperableTransfer(user, userPrivKey, toAddress, value) {
+    const callData = interop.methods.interoperableTransfer(toAddress, value).encodeABI();
+    const rawTx = await createRawTx(userPrivKey, user, config.interopAddress, callData);
+
+    console.log("0x"+rawTx.toString('hex'));
+    return web3.eth.sendSignedTransaction("0x"+rawTx.toString('hex'));
+}
+
+async function withdraw(withdrawer, withdrawerPrivKey, toAddress, value) {
+    const callData = interop.methods.withdrawTST2(toAddress, value).encodeABI();
+    const rawTx = await createRawTx(withdrawerPrivKey, withdrawer, config.interopAddress, callData);
+
+    console.log("0x"+rawTx.toString('hex'));
+    return web3.eth.sendSignedTransaction("0x"+rawTx.toString('hex'));
 }
 
 async function checkResults() {
@@ -49,19 +102,24 @@ async function checkResults() {
     console.log("user2Token2: "+ user2Token2)
     console.log("interopToken1: "+ interopToken1)
     console.log("interopToken2: "+ interopToken2)
+    return;
 }
 
 sendToken1(config.user1, initialUser1Balance).then(() => {
+    console.log("sent tokens to user 1");
     sendToken2(config.interopAddress, initialInteropToken2Balance).then(() => {
-        approveToken1(config.user1, config.interopAddress, tokenInteropTransferValue).then(() => {
+        console.log("Added liquidity to interoperability contract");
+        approveToken1(config.user1, config.user1PrivKey, config.interopAddress, tokenInteropTransferValue).then(() => {
             console.log("approved Tokens")
-            interop.methods.interoperableTransfer(config.user2, tokenInteropTransferValue).send({
-                from: config.user1,
-                gas: 99999999
+            return interoperableTransfer(config.user1, config.user1PrivKey, config.user2, tokenInteropTransferValue)
+            // interop.methods.interoperableTransfer(config.user2, tokenInteropTransferValue).send({
+            //     from: config.user1,
+            //     gas: 99999999
+            // }).then(console.log);
+        }).then(() => {
+            checkResults().then(() => {
+                withdraw(config.owner, config.ownerPrivKey, config.owner, 200).then(console.log);
             })
-        })
+        });
     })
 });
-
-
-setTimeout(checkResults, 3000);
--- truffle-config.js
+++ truffle-config.js
@@ -1,9 +1,18 @@
+var WalletProvider = require("truffle-hdwallet-provider");
+
+var wallet = require('ethereumjs-wallet').fromPrivateKey(new Buffer("4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d", 'hex'));
+var mnemonic = "myth like bonus scare over problem client lizard pioneer submit female collect";
 module.exports = {
   networks: {
     development: {
-      host: "127.0.0.1",
+      host: 'localhost',
       port: 8545,
+      network_id: '*' // Match any network id
+    },
+    ropsten: {
+      provider: () => { return new WalletProvider(mnemonic,"https://ropsten.infura.io");},
+      gas: "4600000",
       network_id: "*"
     }
   }
-}
+};
